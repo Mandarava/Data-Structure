188328c7ca8b85afeb0600c8f90fc76045d0d4ad
那年初夏 <569045298@qq.com>
2014-07-18 17:13:05 +0800

    修改

--- 课程设计/主菜单.cpp
+++ 课程设计/主菜单.cpp
@@ -4,30 +4,28 @@
 #include<stdlib.h> 
 void main()
 {
-int i,flag=1;
-while (flag)
-{system("cls");
-L1:printf("\n\t数据结构课程设计\n");
-	printf("\t1:二叉树的遍历\n");
-	printf("\t2:各种排序\n");
-	printf("\t3:汉诺塔问题\n");
-	printf("\t0:退出\n");
-	printf("\t请选择0--3:\n");
-	while (true)
-	{
-		scanf("%d",&i);
-		if (i>=0 && i<=3)
-			break;
-		else
-			printf("输入错误，");
-			printf("请选择0--3:\n");
+	int i;
+	while (1)
+	{	
+		system("cls");
+		printf("\n\t数据结构课程设计\n");
+		printf("\t1:二叉树的遍历\n");
+		printf("\t2:各种排序\n");
+		printf("\t3:汉诺塔问题\n");
+		printf("\t0:退出\n");
+		printf("\t请选择0--3:\n");
+		while (1)
+		{
+			scanf("%d",&i);
+			if (i>=0 && i<=3)
+				break;
+		}
+		switch (i)
+		{
+		case 1:{list2();break;}
+		case 2:{list1();break;}
+		case 3:{list3();break;}
+		case 0:{return 0;}
+		}
 	}
-	switch (i)
-	{
-	case 1:{list2();break;}
-	case 2:{list1();break;}
-	case 3:{list3();break;}
-	case 0:{flag=0;break;}
-	}
-}
 }
